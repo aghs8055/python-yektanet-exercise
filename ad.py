d8f3c3fc783a581973a8978c24af244b8834240f
Ali Ghaffari Sabet <aghs8055@gmail.com>
2021-10-06 20:59:57 +0330

    Ad class creater with specified fields and methods.

---
+++ ad.py
@@ -0,0 +1,34 @@
+from base_model import BaseModel
+
+
+class Ad(BaseModel):
+    def __init__(self, id, title, img_url, link, advertiser):
+        super().__init__(id)
+        self.__title = title
+        self.__img_url = img_url
+        self.__link = link
+        self.__advertiser = advertiser
+
+    def get_title(self):
+        return self.__title
+
+    def set_title(self, title):
+        self.__title = title
+
+    def get_img_url(self):
+        return self.__img_url
+
+    def set_img_url(self, img_url):
+        self.__img_url = img_url
+
+    def get_link(self):
+        return self.__link
+
+    def set_link(self, link):
+        self.__link = link
+
+    def set_advertiser(self, advertiser):
+        self.__advertiser = advertiser
+
+    def describeMe(self):
+        print('This class contains fields and methods related to advertising.')
